8519033488c39f60684ca3dc4a2548512ba039fd
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-20 01:42:20 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('application one requested', function (req, res) {
+  res.sendFile('application one requested and served here');
 });
 
 app.get('/ui/madi.png', function (req, res) {
