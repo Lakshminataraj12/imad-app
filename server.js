1bf2dd55e898bbd1882df63bc0fb5d5b42cf6ab9
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-21 08:43:32 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,32 +4,6 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});   
-    
-app.get('/article-one', function (req, res) {
-   res.sendFile('Article one requested and will be served here');
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile('Article two requested and will be served here');
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile('Article three requested and will be served here');
-});
-
-app.get('/ui/style.css', function (req, res) {
-    res.sendFile(path.join(__dirname,'ui','style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
 var port = 8080;
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
