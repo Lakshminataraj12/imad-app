2e3b17b4e448a5c860d1a114c3eab12f6e468e0e
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-21 23:54:21 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/artcile-one', function (req, res) {
+     res.sendFile('Article one requested and will be served here');
 });
 
+app.get('/artcile-two', function (req, res) {
+     res.sendFile('Article two requested and will be served here');
+});
+
+app.get('/artcile-three', function (req, res) {
+     res.sendFile('Article three requested and will be served here');
+});
 app.get('/ui/madi.png', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
