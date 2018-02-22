57f61d6ba278be00d485c6a238003104a49cd4ec
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-22 07:50:54 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artcile-one', function (req, res) {
+app.get('/article-one', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     });
 
-app.get('/artcile-two', function (req, res) {
+app.get('/article-two', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-two.htmll'));
 });
 
-app.get('/artcile-three', function (req, res) {
+app.get('/article-three', function (req, res) {
        res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
