1baf78daad631b30c297bf8f6ec7aa60efd5cdd7
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-26 15:23:10 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 var articleTwo= {
     title: 'Article Two | Lakshmi Natraj',
     heading: 'Article Two',
-    date: 'Feb 26, 2019', 
+    date: 'Feb 26, 2018', 
     content: `
               <p>
           This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
@@ -33,7 +33,6 @@ var articleTwo= {
     <head>
         <title>
         ${title}
-            Article Two | Lakshmi Natraj
         </title>
     </head>
     <body>
