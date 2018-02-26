38b502ab7a7799f50b2628d84e1880676f4e5f25
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-26 14:40:02 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ var articleTwo= {
        ${content}     
                     </div>
     </body>
-     </html>                   
+     </html>'                  
             }
             
  return htmlTemplate;
