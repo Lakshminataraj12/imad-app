54d6e556b0c19efa6867832ee99867c832bc2f86
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-27 02:03:06 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ var articleTwo= {
  var htmlTemplate  = '
  <html>
     <head>
-        <title>
+        <title> 
+        <meta name="viewport" content="width-device-width, initialscale=1" />
+           <link href="/ui/style.css" rel="stylesheet.css" />
         ${title}
         </title>
     </head>
