7917a0a671cb1325b22c764ea8149a510f045a14
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-27 21:32:47 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,45 +22,45 @@ var articleTwo= {
               </p> '
                 };
             
-            fuction createTemplate (data) {
-                var title = data.title;
-                var date = data.date;
-                var heading =data.heading;
-                var content =data.content;
-                
- var htmlTemplate  = '
- <html>
-    <head>
-        <title> 
-       ${title}
-         </title>
-        <meta name="viewport" content="width-device-width, initial-scale=1" />
-           <link href="/ui/style.css" rel="stylesheet.css" />
-         </head>
-    <body>
-    <div class="container">
-        <div>
-            <a href="/">Home</a>
+        fuction createTemplate (data) {
+            var title = data.title;
+            var date = data.date;
+            var heading =data.heading;
+            var content =data.content;
+            
+var htmlTemplate  = '
+<html>
+<head>
+    <title> 
+   ${title}
+     </title>
+    <meta name="viewport" content="width-device-width, initial-scale=1" />
+       <link href="/ui/style.css" rel="stylesheet.css" />
+     </head>
+<body>
+<div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+    </h3>
+    <div>
+        ${date}
         </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-            </div>
- <div>
-       ${content}     
-                    </div>
-                    </div>
-    </body>
-     </html> '                  
-       ;
-        return htmlTemplate;
-            }
+<div>
+   ${content}     
+                </div>
+                </div>
+</body>
+ </html> '                  
+   ;
+    return htmlTemplate;
+        }
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
@@ -68,23 +68,23 @@ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) { 
- res.send(createTemplate(articletwo);   
-  });
+res.send(createTemplate(articletwo);   
+});
 
 app.get('/article-three', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));     
 });
 
 app.get('/ui/style.css', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(`IMAD course app listening on port ${port}!`);
 });
