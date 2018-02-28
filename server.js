3046e0f8e92b7b5fd34c95136ddf1b29525f4dde
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-02-28 00:19:12 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,58 +7,11 @@ app.use(morgan('combined'));
 
 
 var articleTwo= {
-    title:' Article One | Lakshmi Natraj',
+    title: 'Article Two | Lakshmi natraj',
     heading: 'Article One',
     date: 'Feb 28, 2018', 
     content: `
-     <p>
-          This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
-                </p>
-                <p>
-                     This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
-                 </p>
-                <p>
-                         This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
-              </p>'
-                }
-                ;
-            
-function createTemplate (data){
-var title = data.title;
-var date = data.date;
-var heading = data.heading;
-            var content = data.conten;
-            
-var htmlTemplate  ='
-<html>
-<head>
-    <title> 
-   ${title}
-     </title>
-    <meta name="viewport" content="width-device-width, initial-scale=1" />
-       <link href="/ui/style.css" rel="stylesheet.css" />
-     </head>
-<body>
-<div class="container">
-    <div>
-        <a href="/">Home</a>
-    </div>
-    <hr/>
-    <h3>
-        ${heading}
-    </h3>
-    <div>
-        ${date}
-        </div>
-<div>
-   ${content}     
-                </div>
-                </div>
-</body>
- </html>'                  
-   ;
-    return htmlTemplate;
-}
+     
 
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -74,10 +27,6 @@ pp.get('/article-two', function (req, res) {
 });
 
 app.get('/article-three', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));     
-});
-
-app.get('/ui/style.css', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
