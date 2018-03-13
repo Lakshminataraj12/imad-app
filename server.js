413f465bb94d03a838160a0b5a4b9885220e62d6
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-12 21:12:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,108 +1,101 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-
-            var articleTwo= {
-                title: 'Article Two | Lakshmi natraj',
-                heading: 'Article One',
-                date: 'Feb 28, 2018', 
-            content: `    
-            <p>
-                            This is the content for my le.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
-                            </p>
-                            <p>
-                            This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
-                           </p>
-                            <p>
-                            This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
-                        </p>`
-                        };
-                        
-                        
-                            var articlethree= {
-                    title: 'Article Three | Lakshmi natraj',
-                    heading: 'Article three',
+    var express = require('express');
+    var morgan = require('morgan');
+    var path = require('path');
+    
+    var app = express();
+    app.use(morgan('combined'));
+    
+    
+                var articleTwo= {
+                    title: 'Article Two | Lakshmi natraj',
+                    heading: 'Article One',
                     date: 'Feb 28, 2018', 
-                    content:`
-                                  <p>
-                                    This is the content for my third article.  This is the content for my third article.  This is the content for my third article.  
-                                    </p>
-                                    <p>
-                                    This is the content for my third article.  This is the content for my third article.  This is the content for my third article.  
-                                    </p>  <p>
-                                    This is the content for my third article.  This is the content for my third article.  This is the content for my third article.  
-                                    </p>`
-                                     };
-                 
-                 
-                 function createTemplate (data) {
-                     var title = data.title;
-                     var data = data.date;
-                    var heaading = data.heading;
-                    var content = data.content;
-                    
-                 var htmlTemplate =  '
-                 <html>
-                 <head>
-                 <title>
-                 ${title}
-                 </title>
-                 <meta name="viewport" content="width=device-width, initialscale=1" />
-                    <link href="/ui/style.css" rel="stylesheet"/> 
-                 </head>
-                 <body>
-                 <div class="container">
-                 <div>       
-                            <a href="/">Home</a>
-                        </div>
-                        <hr/>
-                        <h3>
-                            ${heading}
-                        </h3>
-            <div>
-                ${date}
-                </div>
-     <div>
-              ${content}
-              </div>
-              </div>
-              </body>
-              </html>
-             ' ;
-              return htmlTemplate;
-     }
-     
-     app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-    });
-    
-    var counter = 0;
-    app.get('/counter',function (req, res) {
-        counter = counter + 1;
-        res.send(counter.tostring());
+                content: `    
+                <p>
+                                This is the content for my le.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
+                                </p>
+                                <p>
+                                This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
+                               </p>
+                                <p>
+                                This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
+                            </p>`
+                            },
+                            'article-three': {
+                        title: 'Article Three | Lakshmi natraj',
+                        heading: 'Article three',
+                        date: 'Feb 28, 2018', 
+                        content:`
+                                      <p>
+                                        This is the content for my third article.  This is the content for my third article. 
+                                        </p>`
+                            }
+                            };
+                     
+                     
+                     function createTemplate (data) {
+                         var title = data.title;
+                         var data = data.date;
+                        var heaading = data.heading;
+                        var content = data.content;
+                        
+                     var htmlTemplate = 
+                     <html>
+                     <head>
+                     <title>
+                     ${title}
+                     </title>
+                     <meta name="viewport" content="width=device-width, initialscale=1" />
+                        <link href="/ui/style.css" rel="stylesheet"/> 
+                     </head>
+                     <body>
+                     <div class="container">
+                     <div>       
+                                <a href="/">Home</a>
+                            </div>
+                            <hr/>
+                            <h3>
+                                ${heading}
+                            </h3>
+                <div>
+                    ${date}
+                    </div>
+         <div>
+                  ${content}
+                  </div>
+                  </div>
+                  </body>
+                  </html>
+                 ';
+                  return htmlTemplate;
+         }
+         
+         app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+        });
+        
+        var counter = 0;
+        app.get('/counter', function (req, res) {
+            counter = counter + 1;
+            res.send(counter.tostring());
+        }};
+        
+        app.get('/:articleName', function (req, res) {
+            //articlename--article-two
+            //articles[articlename] -- {}content object for article two
+    var articleName = req.param.articleName;
+    res.send(createTemplate(articles[articlename]));
+        }};
+         
+      app.get('/ui/style.css', function (req, res) {
+        res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+        });
+        
+         app.get('/ui/main.js', function (req, res) {
+        res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+        });
+        
+        
+        app.get('/ui/madi.png', function (req, res){
+         res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
     }};
-    
-    app.get('/:articleName', function (req, res) {
-        //articlename--article-two
-        //articles[articlename] -- {}content object for article two
-var articleName = req.param.articleName;
-res.send(createTemplate(articles[articlename]));
-    }};
-     
-
-    app.get('/ui/style.css', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-    });
-    
-     app.get('/ui/main.js', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-    });
-    
-    
-    app.get('/ui/madi.png', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-}};
