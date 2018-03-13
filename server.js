b95a7481026a6cfaa7f072740a14e9cc4874bde0
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-12 20:55:40 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ var articleTwo= {
     title: 'Article Two | Lakshmi natraj',
     heading: 'Article One',
     date: 'Feb 28, 2018', 
-    content: `
-    <p>
+content:'    <p>
                 This is the content for my le.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article. 
                 </p>
                 <p>
