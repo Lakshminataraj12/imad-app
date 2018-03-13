a706921b54b951fd4bf256edd5c8ff6502de89bb
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-13 00:02:37 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@
                         title: 'Article Three | Lakshmi natraj',
                         heading: 'Article three',
                         date: 'Feb 28, 2018', 
-                        content:'
+                        content: '
                                       <p>
                                         This is the content for my third article.  This is the content for my third article. 
                                         </p>'
@@ -87,21 +87,20 @@
     res.send(createTemplate(articles[articlename]));
         }};
          
-      app.get('/ui/style.css', function (req, res) {
+            app.get('/ui/style.css', function (req, res) {
         res.sendFile(path.join(__dirname, 'ui', 'style.css'));
         });
         
-         app.get('/ui/main.js', function (req, res) {
+        app.get('/ui/main.js', function (req, res) {
         res.sendFile(path.join(__dirname, 'ui', 'main.js'));
         });
         
-        
         app.get('/ui/madi.png', function (req, res){
          res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-    }};
+    });
     
     var port = 80;
     app.listen(port, function () {
         console.log(`IMAD course app listening on port ${port}!`);
     });
-    }
+    
