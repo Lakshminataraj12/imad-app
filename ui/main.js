fd321227b124debc998108414efc749395e82f6c
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-16 09:31:48 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ request.onreadystatechange = function () {
 if (request.readyState == XMLHttpRequest.DONE) {
     // Take some action
     if (request.status ==200) {
-        alert('logged in successfully');
+        alert('Logged in successfully');
     }else if(request.status == 403) {
         alert('username/password is incorrect');
     }else if(request.status == 500) { 
