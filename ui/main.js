3042e5542036093446d42bba9af9492ed231e40c
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-16 04:22:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ if (request.readyState == XMLHttpRequest.DONE) {
 
     // Make the request
     request.open('GET', 'http://lakshmiobh33.imad.hasura-app.io/counter',true);
-    request.send(null)
+    request.send(null);
     };
 
 // Submit name
