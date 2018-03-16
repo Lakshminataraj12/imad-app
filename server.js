90ff2c1d3519fbcf4ca99564eb7a3ccc0760658b
Lakshmi G N <36024643+Lakshminataraj12@users.noreply.github.com>
2018-03-16 00:06:45 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@
     function hash (input, salt) {
         // How do we create a hash?
         var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-        return hashed.tostring('hex');
+        return hashed.toString('hex');
         }
         
         
